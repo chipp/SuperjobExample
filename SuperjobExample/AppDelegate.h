1a587abbf81b1a154a6e84c544f97449c26dc50b
Vladimir Burdukov <chippcheg@gmail.com>
2015-07-13 12:03:30 +0300

    Initial Commit

---
+++ SuperjobExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperjobExample
+//
+//  Created by Vladimir Burdukov on 13.07.15.
+//  Copyright (c) 2015 Superjob.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperjobExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperjobExample
+//
+//  Created by Vladimir Burdukov on 13.07.15.
+//  Copyright (c) 2015 Superjob.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
